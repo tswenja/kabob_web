68cc7b694874eccace0a303b1f46fbb8818c4d37
Tsai Wenja <agon978@gmail.com>
2018-09-12 19:07:47 +0800

    first commit

---
+++ js/header_effects.js
@@ -0,0 +1,21 @@
+var $head = $( '.header-effects' );
+$( '.ha-waypoint' ).each( function(i) {
+  var $el = $( this ),
+    animClassDown = $el.data( 'animateDown' ),
+    animClassUp = $el.data( 'animateUp' );
+
+  $el.waypoint( function( direction ) {
+    if( direction === 'down' && animClassDown ) {
+      if ( animClassDown === 'none' )
+        $head.attr('class', "header-effects");
+      else
+        $head.attr('class', "header-effects header-effects-on " + animClassDown);
+    }
+    else if( direction === 'up' && animClassUp ){
+      if ( animClassUp === 'none' )
+        $head.attr('class', "header-effects");
+      else
+        $head.attr('class', "header-effects header-effects-on " + animClassUp);
+    }
+  }, { offset: '100%' } );
+} );
