8102a7fbe2bf0a8b713524a949b6dad5ccf64a41
Tsai Wenja <agon978@gmail.com>
2018-09-14 13:04:01 +0800

    Fix mobilemenu bugs.

--- vendor/sidebar/js/jquery.mobilemenu.js
+++ vendor/sidebar/js/jquery.mobilemenu.js
@@ -1,10 +1,11 @@
-(function($) {
+  (function($) {
 
     $.mobilemenu = function(options) {
 
         var settings = $.extend({
             menu: '#mobilemenu',
-            trigger: '#mobilemenu button.trigger'
+            trigger: '#mobilemenu button.trigger',
+            container: 'body'
         }, options);
 
         $(settings.menu).remove();
@@ -13,10 +14,14 @@
             if (!$(this).hasClass('active')) {
                 $(this).addClass('active');
                 $(settings.menu).addClass('active');
+                $(settings.container).addClass('mobilemenu-active');
                 return $('body').css('overflow', 'hidden');
             } else {
                 $(this).removeClass('active');
                 $(settings.menu).removeClass('active');
+                setTimeout(function() {
+                  $(settings.container).removeClass('mobilemenu-active');
+                },500);
                 return $('body').css('overflow', 'auto');
             }
         });
