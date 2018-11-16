b9b6b12b5c41bbc1bbe8114bca7ff18f47db01c1
Tsai Wenja <agon978@gmail.com>
2018-11-16 18:11:09 +0800

    for debug

--- js/components_import.js
+++ js/components_import.js
@@ -3,7 +3,6 @@ components.forEach(function(component) {
   if ('import' in component) {
     var template = component.import.querySelector('template');
     document.querySelector(template.getAttribute('for')).innerHTML = template.innerHTML;
-    console.log(1, component.onload);
     if (component.onload) component.onload();
   }
   else {
@@ -11,7 +10,6 @@ components.forEach(function(component) {
     $.get( component.href , function(data) {
       var template = $(data);
       document.querySelector(template.attr('for')).innerHTML = template.html();
-    console.log(2, component.onload);
       if (component.onload) component.onload();
     });
   }
